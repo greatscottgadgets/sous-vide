e907bfebaa50dda5a6876adc055aaf8a0738e6de
Dominic Spill <dominicgs@gmail.com>
2018-06-01 11:22:19 -0600

    Basic outline for filler firmware

---
+++ greatfet/filler/main.c
@@ -0,0 +1,24 @@
+/*
+ * This file is part of GreatFET
+ */
+
+#include <stddef.h>
+
+#include <libopencm3/cm3/vector.h>
+#include <libopencm3/lpc43xx/m4/nvic.h>
+
+#include <greatfet_core.h>
+
+
+int main(void) {
+	cpu_clock_init();
+	cpu_clock_pll1_max_speed();
+	pin_setup();
+	rtc_init();
+
+	while(true) {
+		;
+	}
+
+	return 0;
+}

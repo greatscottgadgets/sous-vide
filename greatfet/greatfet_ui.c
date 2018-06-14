3195187dfc9e901ba41dc21e499c2f502f3de40f
Dominic Spill <dominicgs@gmail.com>
2018-06-13 23:09:58 -0600

    Remove unused cmake file and variable from greatfet_ui

--- greatfet/greatfet_ui.c
+++ greatfet/greatfet_ui.c
@@ -1,22 +1,5 @@
 /*
- * Copyright 2016 Dominic Spill <dominicgs@gmail.com>
- *
  * This file is part of GreatFET.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; see the file COPYING.  If not, write to
- * the Free Software Foundation, Inc., 51 Franklin Street,
- * Boston, MA 02110-1301, USA.
  */
 
 #include "greatfet_core.h"
@@ -24,9 +7,6 @@
 #include "gpio_lpc.h"
 #include "pins.h"
 #include <libopencm3/lpc43xx/scu.h>
-
-// Constants from https://github.com/CaptainStouf/raspberry_lcd4x20_I2C
-// Which is released under the GPL2 license
 
 // LCD Address
 #define ADDRESS 0x27
@@ -136,7 +116,7 @@ static uint32_t cook_time = 0;
 static char cook_mode = ' ';
 
 void draw_screen(void) {
-	int group, j, i = 0;
+	int j, i = 0;
 	uint64_t x = temperature;
 	uint64_t y = cook_time;
 	char line1[] = {'D', ':', 0x20, 0x20, 0x20, 0x20, 'M', ':',
